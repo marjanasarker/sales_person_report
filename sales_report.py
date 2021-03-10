06b22ee6a474127c412511689a7ca138a76bab5a
Marjana Sarker <msarker86@gmail.com>
2021-03-10 14:38:17 +0000

    Comment change

--- sales_report.py
+++ sales_report.py
@@ -10,6 +10,7 @@ def melons_sold_salesperson(file_path):
     f = open(file_path)
     for line in f:
         line = line.rstrip()
+        #creating a list and unpacking it's values 
         salesperson_name, total_dollars, melons_sold = line.split('|')
         
         # creating key and value pair to add to empty dictionary
@@ -19,6 +20,7 @@ def melons_sold_salesperson(file_path):
             sales_per_person[salesperson_name]= int(melons_sold)
     
     return sales_per_person
+    #melons_sold_salesperson[salesperson_name]= melons_sold_salesperson(salesperson_name, 0) + int(melons_sold)
 
 def print_sales_report(melons_sold_salesperson):  
 
